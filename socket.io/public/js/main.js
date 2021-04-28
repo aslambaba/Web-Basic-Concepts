cd8ec66bcb27c7673aa121f1b0670b022dcad1e1
Aslam Sarfraz <aslam91r@gmail.com>
2021-04-29 00:26:56 +0500

    Basic Concept of Socket.io Completed

--- socket.io/public/js/main.js
+++ socket.io/public/js/main.js
@@ -1,1 +1,21 @@
 const socket = io();
+
+// This is the listner of conformation messege that send form server side
+socket.on('Conformation-Messege',msg=>{
+    console.log(msg);
+});
+
+// This is the listner to alert everyone that new user is active
+socket.on('Broadcase-Messege', msg=> {
+    alert(msg);
+})
+
+// This is the simple listner that recieve the messege from server side
+socket.on('messege', msg=>{
+    console.log(msg);
+});
+
+// This function is called when button is pressed !
+function Boradcast(){
+    socket.emit('BroadCastMessege', "The Button Was Clicked !!");
+}
--- socket.io/server.js
+++ socket.io/server.js
@@ -11,8 +11,24 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Connection When User hit the URL
 io.on('connection', socket=>{
+
+    // Conformation Messege for Server
     console.log("Socket.IO Connected");
+
+    // The Conformation Messege that send to fronend that Socket.IO is connected
+    socket.emit("Conformation-Messege","Socket.IO Connected Successfully with FrontEnd");
+
+    // This is the broadcast messege that all old user recieve when new user is active or hit the url
+    socket.broadcast.emit('Broadcase-Messege', "New User Active");
+
+    // This is the Reciver that is active when the messege button is press on frontend page
+    socket.on('BroadCastMessege', msg=>{
+        console.log(msg);
+        socket.emit("messege", msg);
+
+    });
 })
 
 server.listen(PORT, ()=>{
